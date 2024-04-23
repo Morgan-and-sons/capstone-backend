04c9a0500f865fcf228a9d8bb1c5ade56b60025a
Louie-cpu <luis.u.m779@gmail.com>
2024-04-23 10:38:19 -0700

    rails models have been validated

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :event_participants
+
+  validates :title, :body, :eventphoto, :eventamount, :grouptotal, :creator, :location, presence: true
 end

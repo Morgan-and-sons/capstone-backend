20eca69633cb1e3f9ce2a0dfd5f31e9a6a0622d3
Louie-cpu <luis.u.m779@gmail.com>
2024-04-24 11:38:17 -0700

    specs for update and delete

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -4,7 +4,7 @@ def index
   render json: event_participant
 end
 
-def create 
+def create
   event_participant = EventParticipant.create(event_participant_params)
   if event_participant.valid?
     render json: event_participant
@@ -13,10 +13,29 @@ def create
   end
 end
 
+def update
+  event_participant = EventParticipant.find(params[:id])
+  event_participant.update(event_participant_params)
+  if event_participant.valid?
+    render json: event_participant
+  else
+    render json: event_participant.errors, status: 422
+  end
+end
 
+def destroy
+  event_participant = EventParticipant.find(params[:id])
+  if event_participant.destroy
+    render json: {}, status: 204
+  else
+    render json: {}, status: 422
+    end
+  end
+end
+
+private
 def event_participant_params
     params.require(:event_participant).permit(:user_id, :event_id, :individual_contributions)
   end
-end
 
 
--- spec/requests/event_participants_spec.rb
+++ spec/requests/event_participants_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "EventParticipants", type: :request do
   email: 'test@example.com',
   password: 'password',
   password_confirmation: 'password'
-  ) 
+  )
 }
 
   let(:event) { user.events.create(
@@ -22,7 +22,7 @@ RSpec.describe "EventParticipants", type: :request do
   )}
 
   describe 'Get/index' do
-    it 'returns a event participants and http success' do 
+    it 'returns a event participants and http success' do
       event_participant = EventParticipant.create(
         user_id: user.id,
         event_id: event.id,
@@ -34,7 +34,7 @@ RSpec.describe "EventParticipants", type: :request do
     end
   end
 
-  describe 'POST #create' do 
+  describe 'POST #create' do
     it 'creates a valid event_participant with a http success' do
       post event_participants_path, params: {
         event_participant: {
@@ -47,6 +47,7 @@ RSpec.describe "EventParticipants", type: :request do
     expect(response).to have_http_status(200)
     expect(event_participant).to be_valid
   end
+end
 
   it 'creates an invalid event_participant' do
     post event_participants_path, params: {
@@ -60,5 +61,66 @@ RSpec.describe "EventParticipants", type: :request do
   expect(response).to have_http_status(422)
   expect(event_participant).to eq(nil)
     end
+
+    describe 'PATCH #update' do
+    it 'updates a valid event_participant with http success' do
+      post event_participants_path, params: {
+        event_participant: {
+          user_id: user.id,
+          event_id: event.id,
+          individual_contributions: 100,
+        }
+      }
+
+      event_participant = EventParticipant.where(user_id: user.id).first
+      patch event_participants_path(event_participant), params: {
+        event_participant: {
+          user_id: user.id,
+          event_id: event.id,
+          individual_contributions: 200,
+        }
+      }
+
+      event_participant = EventParticipant.where(user_id: user.id).first
+      expect(event_participant.user_id).to eq(user.id)
+      expect(event_participant.event_id).to eq(event.id)
+      expect(event_participant.individual_contributions).to eq(individual_contributions)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns an http status error for an invalid update' do
+      post event_participants_path, params: {
+        event_participant: {
+          user_id: user.id,
+          event_id: event.id,
+          individual_contributions: 200,
+        }
+      }
+      event_participant = EventParticipant.where(user_id: user.id).first
+      expect(event_participant).not_to be_nil
+
+      patch event_participant_path(event_participant_id), params: {
+        event_participant: {
+          user_id: nil,
+          event_id: nil,
+          individual_contributions: nil,
+        }
+      }
+        event_participant = EventParticipant.where(user_id: nil).first
+        expect(response).to have_http_status(422)
+    end
   end
+
+  describe 'Delete #destroy' do
+  it 'deletes the event_participant' do
+    event_participant = EventParticipant.create(
+      user_id: user.id,
+      event_id: event.id,
+      individual_contributions: 200,
+    )
+    delete event_participant_path(event_participant)
+    event_participant = EventParticipant.where(user_id: user.id).first
+    expect(event_participant).to eq(nil)
+  end
+end
 end

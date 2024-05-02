99fe9b625b2b0eab6e56d2857d20f9f04420c27a
smorgannicole <morgann.smithh@yahoo.com>
2024-05-02 16:58:33 -0700

    updates to update two tables at once

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   has_many :users, through: :event_participants
 
   validates :title, :body, :eventphoto, :eventamount, :grouptotal, :creator, :location, presence: true
+  accepts_nested_attributes_for :event_participants
 end
